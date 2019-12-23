e3653d54090cdd9dd45b2905f861f93cb8483755
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-12-23 23:20:24 +0530

    auth done

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,14 +3,48 @@ import ReactDOM, { render } from "react-dom";
 
 import Navbar from "./navbar";
 
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <div className="home-banner">
-        <h1 className="home-1">conduit</h1>
-        <p className="home-para"> A Place To Share Knowledge.</p>
+export default class Home extends React.Component {
+  state = {
+    tags: [],
+    articles: [],
+    username: "",
+    password: ""
+  };
+  componentDidMount() {
+    fetch("https://conduit.productionready.io/api/tags")
+      .then(res => res.json())
+      .then(({ tags }) => this.setState({ tags }));
+    fetch("https://conduit.productionready.io/api/articles")
+      .then(res => res.json())
+      .then(({ articles }) => this.setState({ articles }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="home-banner">
+          <h1 className="home-1">conduit</h1>
+          <p className="home-para"> A Place To Share Knowledge.</p>
+        </div>
+        <div>
+          <div>
+            <ul>
+              {this.state.articles.map(article => (
+                <li>{article.title}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              {this.state.tags.map(tag => (
+                <li>{tag}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -4,35 +4,83 @@ import ReactDOM, { render } from "react-dom";
 // import Navbar from "../src/components/navbar";
 import Navbar from "./navbar";
 
-export default function SignIn(props) {
-  return (
-    <div>
-      <Navbar />
-      <div className="main-container">
-        <div className="rows">
-          <h1>Sign In</h1>
-          <p className="text-center">
-            <a className="account" href="#register">
-              Need an account?
-            </a>
-          </p>
-          <form>
-            <fieldset>
-              <fieldset className="email">
-                <input className="input" type="email" placeholder="Email" />
+export default class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      message: ""
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    fetch("https://conduit.productionready.io/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    })
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+
+        if (localStorage.token) {
+          this.props.history.push("/");
+        } else {
+          this.setState({ message: data.message });
+        }
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="main-container">
+          <div className="rows">
+            <h1>Sign In</h1>
+            <p className="text-center">
+              <a className="account" href="#register">
+                Need an account?
+              </a>
+            </p>
+            <form onSubmit={this.handleChange}>
+              <fieldset>
+                <fieldset className="email">
+                  <input
+                    className="input"
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    onChange={this.onChange}
+                  />
+                </fieldset>
+                <fieldset className="email">
+                  <input
+                    onChange={this.onChange}
+                    className="input"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </fieldset>
               </fieldset>
-              <fieldset className="email">
-                <input
-                  className="input"
-                  type="password"
-                  placeholder="Password"
-                />
-              </fieldset>
-            </fieldset>
-            <button className="btn_primary">Sign In</button>
-          </form>
+              <button className="btn_primary">Sign In</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
